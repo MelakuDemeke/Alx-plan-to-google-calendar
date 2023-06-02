7e934050eacada4f995ff0b840b52c951328395f
Melaku Demeke <melaku.demeke789@gmail.com>
2023-06-02 15:10:13 +0300

    change the icons

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -6,3 +6,12 @@ for (let i = 0; i < h1Tags.length; i++) {
   const h1Tag = h1Tags[i];
   h1Tag.innerText = 'Modified H1';
 }
+
+// Find all elements with the class name "dhx_cal_event_line"
+const elements = document.querySelectorAll(".dhx_cal_event_line");
+
+// Loop through each element and add "close" to aria-label
+elements.forEach(element => {
+  const ariaLabel = element.getAttribute("aria-label");
+  element.setAttribute("aria-label", `close ${ariaLabel}`);
+});
